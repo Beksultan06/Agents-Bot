8f35087557fb9d921dd287d6f599ed3c4f365a01
didiotghima <nurlanuuulubeksultan@gmail.com>
2025-02-20 12:49:04 +0600

    add button agents

--- bitton.py
+++ bitton.py
@@ -1,2 +1,9 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+def create_keyboard():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="📱 SSM", callback_data="agent_ssm"),
+         InlineKeyboardButton(text="✍️ Копирайтер", callback_data="agent_copywriter")],
+        [InlineKeyboardButton(text="🎬 Сценарист", callback_data="agent_scriptwriter"),
+         InlineKeyboardButton(text="🤖 GPT", callback_data="agent_cpt")],
+    ])
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from aiogram import Bot, types, Dispatcher
 from aiogram.filters import Command
 import asyncio, logging, os
 from dotenv import load_dotenv
+from bitton import create_keyboard
 
 load_dotenv()
 
@@ -12,7 +13,7 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start(message:types.Message):
-    await message.answer("Привет я бот который предоставляет Агентов!")
+    await message.answer("Привет я бот который предоставляет Агентов!", reply_markup=create_keyboard())
 
 async def main():
     await dp.start_polling(bot)
